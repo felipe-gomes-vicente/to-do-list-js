b4d3817374ade06f1716f527de0924c93edd7064
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-05 11:25:39 -0300

    export default {

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#191818",
+    BACKGROUND_800: "#2B2A2A",
+    BACKGROUND_700: "#3B3B3B",
+
+    WHITE: "#FFD",
+    WHITE_SEC: "#DDD",
+
+    GRAY_100: "#999",
+    GRAY_300: "#555",
+  }
+}
